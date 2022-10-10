3fb18429a07ebb540e9944a1a98fd1e8cdce77fc
Conoisseur <97054370+Conoisseur@users.noreply.github.com>
2022-10-10 21:24:53 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,23 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
+
     list = []
+    list.append(2)
+
+    for i in range(number_of_primes - 1):
+        numberToCheck = list[-1] + 1
+        while not isPrime(numberToCheck):
+            numberToCheck += 1
+        list.append(numberToCheck)
+
     return list
+
+
+def isPrime(number):
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
